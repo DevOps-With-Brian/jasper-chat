c49a71a64b117e0b7f3ecfe382afce8a248b238a
Brian Hopkins <brianhh1230@gmail.com>
2023-02-18 21:56:58 -0500

    feat(snake-temps): adding in the required components for the snake room temp intent

--- actions/actions.py
+++ actions/actions.py
@@ -6,6 +6,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
+from datetime import datetime
 
 load_dotenv()
 
@@ -50,6 +52,64 @@ class ActionAPOD(Action):
         else:
             # If the status code is not 200 (OK), print an error message
             jasper_err = "Sorry, I wasn't able to hit the NASA API at this time."
+            print("An error occurred while fetching the data:", response.text)
+            dispatcher.utter_message(text=jasper_err)
+
+        return []
+
+
+class ActionSnaketemp(Action):
+
+    def name(self) -> Text:
+        return "action_snake_temp"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Fauna GraphQL endpoint
+        now = datetime.now() # current date and time
+        accessToken = os.getenv('FAUNA_KEY')
+        endpoint = f"https://graphql.us.fauna.com/graphql"
+        headers = {
+            'Authorization': f"Bearer {accessToken}"
+        }
+
+        date = now.strftime("%Y-%m-%d")
+
+        query = """query {
+        tempsByDate(date: "%s"){
+            data{
+                _id
+                temp
+                time
+                date
+            }
+        }
+        }""" % (date)
+
+        json_data = {
+            'query': query
+        }
+
+        response = requests.post(endpoint, headers=headers, json=json_data)
+
+        # Check the status code of the response
+        if response.status_code == 200:
+            # If the status code is 200 (OK), get the JSON data from the response
+            temp_results = response.json()['data']
+            temp_list = temp_results['tempsByDate']['data']
+            latest_temp_index = len(temp_list) - 1
+            latest_temp = temp_list[latest_temp_index]['temp']
+            latest_temp_time = temp_list[latest_temp_index]['time']
+
+            # Print the title and explanation of the APOD
+            jasper_response = "The latest temperature reading from the snake room is currently {0}F and was taken at {1}".format(latest_temp, latest_temp_time)
+            dispatcher.utter_message(text=jasper_response)
+
+        else:
+            # If the status code is not 200 (OK), print an error message
+            jasper_err = "Sorry, I wasn't able to hit the graphql endpoint at this time."
             print("An error occurred while fetching the data:", response.text)
             dispatcher.utter_message(text=jasper_err)
 
